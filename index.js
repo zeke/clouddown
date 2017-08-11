b1843f52794ae3bb1eef26a47f0eac5c651c9e44
Zeke Sikelianos <zeke@sikelianos.com>
2017-08-11 10:27:01 -0700

    add pagination with forked cloudup client

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-const path = require('path')
-const osenv = require('osenv')
+require('dotenv-safe').load()
+
 const Cloudup = require('cloudup-client')
-const pkg = require('./package')
+const fs = require('fs')
+const path = require('path')
+const {pick} = require('lodash')
+const mkdirp = require('make-dir').sync
+const client = new Cloudup({
+  user: process.env.EMAIL,
+  pass: process.env.PASSWORD
+})
+let files = []
 
-var opts = require(path.resolve(osenv.home(), '.cloudup.json'))
-opts.useragent = 'clouddown/' + pkg.version
+getPage()
 
-var client = new Cloudup(opts)
+function getPage (page = 1) {
+  const opts = {
+    page: page
+  }
+  client.streams(opts, (err, streams, nextPage) => {
+    if (err) throw err
 
-client.streams(function(err, streams){
-  streams.forEach(function(stream){
-    stream.items(function(err, items){
-      items.forEach(function(item){
-        console.log(`  ${item.direct_url} ${item.filename}`)
-      });
-    });
-  });
-});
+    streams.forEach((stream, i) => {
+      stream.items((err, items) => {
+        if (typeof items === 'undefined' || !items) return
+
+        items.forEach((item, j) => {
+          const {id, stream_id} = item
+          const filename = path.join(__dirname, `metadata/${stream_id}---${id}.json`)
+
+          if (fs.existsSync(filename)) {
+            console.log(`${filename} (exists, skipping)`)
+          } else {
+            fs.writeFileSync(filename, JSON.stringify(item, null, 2))
+            console.log(filename)
+          }
+
+          if (i === streams.length - 1 && j === items.length - 1) {
+            if (nextPage) {
+              console.log('nextPage')
+              getPage(nextPage)
+            } else {
+              console.log('done')
+            }
+          }
+        })
+      })      
+    })
+  })
+}
---
+++ readme.md
@@ -0,0 +1,35 @@
+# clouddown 
+
+Download all your CloudUp files
+
+## Installation
+
+```sh
+npm install clouddown --global
+```
+
+
+## Tests
+
+```sh
+npm install
+npm test
+```
+
+## Dependencies
+
+- [cloudup-client](): cloudup api client
+- [dotenv-safe](https://github.com/rolodato/dotenv-safe): Load environment variables from .env and ensure they are defined
+- [limiter](https://github.com/jhurliman/node-rate-limiter): A generic rate limiter for node.js. Useful for API clients, web crawling, or other tasks that need to be throttled
+- [lodash](https://github.com/lodash/lodash): Lodash modular utilities.
+- [make-dir](https://github.com/sindresorhus/make-dir): Make a directory and its parents if needed - Think `mkdir -p`
+- [parse-link-header](https://github.com/thlorenz/parse-link-header): Parses a link header and returns paging information for each contained link.
+
+## Dev Dependencies
+
+- [standard](https://github.com/standard/standard): JavaScript Standard Style
+
+
+## License
+
+MIT
